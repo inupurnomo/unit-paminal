63e74b89c89b59389b41d0440123c96ed8fc970f
Ilham Ibnu Purnomo <social.inupurnomo@gmail.com>
2024-08-27 15:21:03 +0700

    remove menu

--- resources/views/layouts/commonMaster.blade.php
+++ resources/views/layouts/commonMaster.blade.php
@@ -27,7 +27,7 @@
     <!-- Canonical SEO -->
     <link rel="canonical" href="{{ config('variables.productPage') ? config('variables.productPage') : '' }}">
     <!-- Favicon -->
-    <link rel="icon" type="image/x-icon" href="{{ asset('assets/img/favicon/favicon.ico') }}" />
+    <link rel="icon" type="image/x-icon" href="{{ asset('favicon.ico') }}" />
     @notifyCss
     <style type="text/css"> .notify{ z-index: 1000000; margin-top: 45px; } </style>
     <style>
